3cf66071d9a0de947a7faf7d74b7fce7a3429d87
jbadan <jenna.badanowski@gmail.com>
2019-04-10 11:05:22 -0700

    log file names

--- scripts/diffCss.js
+++ scripts/diffCss.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 let file = process.argv[2].replace(/dist2/gi, 'dist');
-
+console.log(process.argv[2]);
+console.log(file);
 require("css-diff")({
     files: [
         process.argv[2],
